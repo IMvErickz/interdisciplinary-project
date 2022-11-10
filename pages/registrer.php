03495d79a7f559ef96abb5893634eb6ad0bb0e22
IMvErickz <erickspy2003@gmail.com>
2022-11-10 17:57:45 -0300

    modified design

--- pages/registrer.php
+++ pages/registrer.php
@@ -12,78 +12,78 @@
     <title>Cadastro</title>
 </head>
 
-<body>
-    <div class="flex flex-col justify-center items-center w-screen h-screen bg-background">
-        <header class="flex flex-row justify-center items-center gap-x-4">
-            <img src="../assets/incons/open-book 1.png" alt="" class="w-16 h-16">
-            <div class="flex flex-col justify-center items-center">
-                <h1 class="text-buttonColor text-2xl">PsychoMango</h1>
-                <span class="flex flex-col justify-center text-center text-label">Registre-se para aproveitar <br>
-                    todos nossos serviços
-                </span>
+<body class="bg-background">
+
+    <header class="flex flex-row justify-center items-center gap-x-4">
+        <img src="../assets/incons/open-book 1.png" alt="" class="w-28 h-28">
+        <div class="flex flex-col justify-center items-center">
+            <h1 class="text-buttonColor text-3xl">PsychoMango</h1>
+            <span class="flex flex-col justify-center text-center text-label">Registre-se para aproveitar <br>
+                todos nossos serviços
+            </span>
+        </div>
+    </header>
+
+    <div class="flex flex-col justify-center items-center">
+        <div class="flex flex-col justify-start w-80">
+            <label for="cpf" class="text-labelInput text-base font-semibold">CPF</label>
+            <input type="text" id="cpf" placeholder="123.456.789-10" class="bg-label rounded-[15px] text-white py-3 px-4 w-full"> <br>
+
+            <label for="cpf" class="text-labelInput text-base font-semibold">Email</label>
+            <input type="email" id="email" placeholder="email@example.com" class="bg-label rounded-[15px] text-white py-3 px-4"> <br>
+            <label for="cpf" class="text-labelInput text-base font-semibold">Sua senha</label>
+            <input type="password" id="password" placeholder="********" class="bg-label rounded-[15px] text-white py-3 px-4"> <br>
+
+            <label for="cpf" class="text-labelInput text-base font-semibold">Confirme sua senha</label>
+            <input type="password" id="" placeholder="********" class="bg-label rounded-[15px] text-white py-3 px-4"> <br>
+        </div>
+
+        <div class="flex flex-col justify-start w-80">
+            <div class="flex flex-row justify-start w-80 gap-32">
+                <label for="cpf" class="text-labelInput text-base font-semibold">Nome</label>
+                <label for="cpf" class="text-labelInput text-base font-semibold">Sobrenome</label>
             </div>
-        </header>
 
-        <div class="flex flex-col justify-center items-center w-screen h-screen">
-            <div class="flex flex-col justify-start w-60">
-                <label for="cpf" class="text-labelInput text-base font-semibold">CPF</label>
-                <input type="text" id="cpf" placeholder="123.456.789-10" class="bg-label rounded-[15px] text-white p-1">
-
-                <label for="cpf" class="text-labelInput text-base font-semibold">Email</label>
-                <input type="email" id="email" placeholder="email@example.com" class="bg-label rounded-[15px] text-white p-1">
-                <label for="cpf" class="text-labelInput text-base font-semibold">Sua senha</label>
-                <input type="password" id="cpf" placeholder="********" class="bg-label rounded-[15px] text-white p-1">
-
-                <label for="cpf" class="text-labelInput text-base font-semibold">Confirme sua senha</label>
-                <input type="password" id="cpf" placeholder="********" class="bg-label rounded-[15px] text-white p-1">
+            <div class="flex flex-row justify-start w-80 gap-x-2">
+                <input type="text" id="name" placeholder="Nome" class="bg-label rounded-[15px] text-white py-3 px-4 w-[150px]"> <br>
+                <input type="text" id="lastName" placeholder="Sobrenome" class="bg-label rounded-[15px] text-white py-3 px-4 w-[150px]">
             </div>
 
-            <div class="flex flex-col justify-start w-60">
-                <div class="flex flex-row justify-start w-60 gap-20">
-                    <label for="cpf" class="text-labelInput text-base font-semibold">Nome</label>
-                    <label for="cpf" class="text-labelInput text-base font-semibold">Sobrenome</label>
-                </div>
-
-                <div class="flex flex-row justify-start w-60 gap-x-2">
-                    <input type="text" id="name" placeholder="Nome" class="bg-label rounded-[15px] text-white p-1 w-[115px]">
-                    <input type="text" id="lastName" placeholder="Sobrenome" class="bg-label rounded-[15px] text-white p-1 w-[115px]">
-                </div>
-
-                <div class="flex flex-col justify-start">
-                    <label for="" class="text-labelInput text-base font-semibold">Pais</label>
-                    <select name="" id="" class="bg-label rounded-[15px] p-1">
-                        
-                    </select>
-                </div>
-            </div>
-            
-            
-                <div class="flex flex-row justify-center w-full gap-20">
-                    <label for="cpf" class="text-labelInput text-base font-semibold">Data de nascimento</label>
-                    <label for="cpf" class="text-labelInput text-base font-semibold">Sexo</label>
-                </div>
-
-                <div class="flex flex-row justify-center w-60 gap-2">
-                    <input type="date" placeholder="10/09/2000" class="bg-label rounded-[15px] text-white p-1 w-[115px]">
-                    <input type="text" placeholder="Masc/Fem/Outro" class="bg-label rounded-[15px] text-white p-1 w-[115px]">
-                </div>
-            
             <div class="flex flex-col justify-start">
-                <div class="flex flex-row gap-1">
-                    <input type="checkbox">
-                    <span class="text-checkLabel">Eu li e estou ciente sobre as</span>
-                    <span class="underline-offset-2 text-label">políticas de privacidade.</span>
-                </div>
-                <div class="flex flex-row gap-1">
-                    <input type="checkbox">
-                    <span class="text-checkLabel">Eu li e aceito os</span>
-                    <span class="text-label"><a href="" class="underline-offset-2 ">termos e condições.</a></span>
-                </div>
+                <label for="" class="text-labelInput text-base font-semibold">Pais</label>
+                <select name="" id="" class="bg-label rounded-[15px] py-3 px-4"></select> <br>
             </div>
+        </div>
+
+
+        <div class="flex flex-row justify-center w-80 gap-20">
+            <label for="cpf" class="text-labelInput text-base font-semibold">Data de nascimento</label>
+            <label for="cpf" class="text-labelInput text-base font-semibold">Sexo</label>
+        </div>
+
+        <div class="flex flex-row justify-center w-80 gap-2">
+            <input type="date" id="dateNasc" placeholder="10/09/2000" class="bg-label rounded-[15px] text-white py-3 px-4 w-[150px]">
+            <select name="" id="sexo" class="bg-label rounded-[15px] py-3 px-4 w-[150px]"></select>
+        </div> <br>
+
+        <div class="flex flex-col justify-start">
+            <div class="flex flex-row gap-1">
+                <input type="checkbox">
+                <span class="text-checkLabel">Eu li e estou ciente sobre as</span>
+                <span class="underline-offset-2 text-label">políticas de privacidade.</span>
+            </div>
+            <div class="flex flex-row gap-1">
+                <input type="checkbox">
+                <span class="text-checkLabel">Eu li e aceito os</span>
+                <span class="text-label"><a href="" class="underline-offset-2 ">termos e condições.</a></span>
+            </div> <br>
+        </div>
+        <div class="flex flex-col justify-center items-center gap-7">
             <button id="but" class="bg-amareloMango w-96 rounded-[15px] py-2 px-1">Cadastrar</button>
             <a href="" class="text-label text-base">Já tem uma conta?</a>
         </div>
     </div>
+
 </body>
 <script src="../src/registre.js"></script>
 <style>
--- src/registre.js
+++ src/registre.js
@@ -1,4 +1,8 @@
 const regisClient = {
     Nome: document.getElementById("name").value, 
-    Sobrenome: document.getElementById("lastName").value
+    Sobrenome: document.getElementById("lastName").value,
+    CPF: document.getElementById('cpf').value,
+    email: document.getElementById("email").value,
+    Data: document.getElementById('dateNasc').value,
+    Sexo: document.getElementsById('sexo')
 }
