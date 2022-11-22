48846ed40b2112b56764fdb7a481a26989d1fe18
IMvErickz <erickspy2003@gmail.com>
2022-11-21 21:22:51 -0300

    refatoring

--- src/beforeRedirect.js
+++ src/beforeRedirect.js
@@ -1,16 +1,11 @@
 window.onload = function() {
     
     let recName = localStorage.getItem("personName")
-    let noneLogin = document.getElementById("login")
-    let noneRegistre = document.getElementById("res")
+    let noneLogin = document.getElementById("buts")
     let span = document.getElementById('setName')
     let avatar = document.getElementById("avatar")
 
-    
-    
-    
     if (recName != undefined) {
-         noneLogin = document.getElementById("buts")
     noneLogin.style.display = "none"
     
       span = document.getElementById('setName')
@@ -18,13 +13,11 @@ window.onload = function() {
         span.innerHTML = "Ola, " + recName
         avatar.src = "./assets/incons/avatar.png"
     } else {
-          noneLogin = document.getElementById("buts")
     noneLogin.style.display = "block"  
 
         span = document.getElementById('setName')
         span.innerHTML = ""
         avatar.src = ""
-
 
     }
     
