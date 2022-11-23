501e041f39b198505256396062a3605ffa8805b4
IMvErickz <erickspy2003@gmail.com>
2022-11-23 11:28:10 -0300

    fixed: query name for client

--- server/login/login.php
+++ server/login/login.php
@@ -1,18 +1,20 @@
 <?php
 function setBut()
 {
+    
     $but  = isset($_POST["send"]);
     if ($but) {
         include_once '../server/BDconection.php';
         $myBank = conection();
 
-        $sql = "select * from person";
+        $valueEmail = filter_input(INPUT_POST, "email");
+
+        $sql = "select * from person where email = '$valueEmail'";
 
         $result = $myBank->query($sql);
 
         $list = $result->fetch(PDO::FETCH_ASSOC);
 
-        $inputEmail = filter_input(INPUT_POST, "email");
         $inputPass = filter_input(INPUT_POST, "pass");
 
         $email = $list['email'];
@@ -20,11 +22,11 @@ function setBut()
         $name = $list['pes_nome'];
 
 
-        if ($email != $inputEmail || $password != $inputPass) {
+        if ($email != $valueEmail || $password != $inputPass) {
             echo "<span class='text-red-600 font-semibold text-3xl'>";
             echo "Email ou senha incorretos";
             echo "</span>";
-        } else if ($inputEmail == "" || $inputPass == "") {
+        } else if ($valueEmail == "" || $inputPass == "") {
             echo "<span class='text-red-600 font-semibold text-3xl'>";
             echo "Os campos estão vazios";
             echo "</span>";
